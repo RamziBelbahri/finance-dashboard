3e7e392d86ace87621cffb5c5af9141f7939ae19
Ramzi Belbahri <Ramzi.Belbahri@fdmgroup.com>
2026-05-21 16:27:05 -0400

    Added JPA and psql set up

---
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.backend.controller;
+
+import com.example.backend.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    private final UserRepository userRepo;
+
+    public UserController(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping()
+    public String getAllUsers() {
+        return "OK";
+    }
+}

628652b6f474750c3f2e472fa626a935ae4c36b5
Ramzi Belbahri <Ramzi.Belbahri@fdmgroup.com>
2026-05-23 17:22:16 -0400

    added create and read transactions

--- backend/src/main/java/com/example/backend/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/backend/security/JwtAuthenticationFilter.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.rmi.ServerException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
---
+++ backend/src/main/java/com/example/backend/transaction/TransactionController.java
@@ -0,0 +1,30 @@
+package com.example.backend.transaction;
+
+import com.example.backend.transaction.dto.CreateTransactionRequest;
+import com.example.backend.transaction.dto.TransactionResponse;
+import com.example.backend.user.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public TransactionResponse createTransaction(@AuthenticationPrincipal User user, @RequestBody CreateTransactionRequest request) {
+        return this.transactionService.createTransaction(request, user);
+    }
+
+    @GetMapping
+    public List<TransactionResponse> getUserTransactions(@AuthenticationPrincipal User user) {
+        return this.transactionService.getUserTransactions(user);
+    }
+}

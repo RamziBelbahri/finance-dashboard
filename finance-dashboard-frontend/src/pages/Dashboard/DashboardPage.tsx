d128db8cb3fcbeaa8655e0b2cce390adf4d66af2
Ramzi Belbahri <Ramzi.Belbahri@fdmgroup.com>
2026-05-24 18:38:59 -0400

    Added first UI look for dashboard

--- finance-dashboard-frontend/src/pages/Dashboard/DashboardPage.tsx
+++ finance-dashboard-frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -27,31 +27,41 @@ export default function DashboardPage() {
   }, []);
 
   return (
-    <>
-      <h2>Dashboard</h2>
-      <div>
-        Create Transaction Form:
-        <CreateTransaction loadTransactions={loadTransactions} />
+    <div className="min-h-screen bg-blue-200 p-6 text-black">
+      <div className="max-w-5x1 mx-auto">
+        <h1 className="text-center text-3xl text-white font-bold bg-blue-700 rounded-xl p-2 mb-4">Finance Dashboard</h1>
+        <div>
+          <CreateTransaction loadTransactions={loadTransactions} />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+          <div className="bg-white rounded-xl p-4 shadow">
+            <h3 className="text-gray-500">Balance</h3>
+            <p className="text-2xl font-bold text-600">${balance.toFixed(2)}</p>
+          </div>
+          <div className="bg-white rounded-xl p-4 shadow">
+            <h3 className="text-gray-500">Income</h3>
+            <p className="text-2xl font-bold text-green-600">${totalIncome.toFixed(2)}</p>
+          </div>
+          <div className="bg-white rounded-xl p-4 shadow">
+            <h3 className="text-gray-500">Expenses</h3>
+            <p className="text-2xl font-bold text-red-600">${totalExpense.toFixed(2)}</p>
+          </div>
+        </div>
+        {
+          transactions.length === 0 && (
+            <div className="bg-white rounded-xl p-6 text-center text-gray-500">
+              No transactions yet.
+            </div>
+          )
+        }
+        {
+          sortedTransactions.map((transaction) => {
+            return <TransactionCard id={transaction.id} amount={transaction.amount} description={transaction.description} transactionDate={transaction.transactionDate}
+              type={transaction.type} key={transaction.id} />
+          })
+        }
       </div>
-      <div>
-        <h3>Balance : ${balance.toFixed(2)}</h3>
-        <h4>Income: ${totalIncome.toFixed(2)}</h4>
-        <h4>Expenses: ${totalExpense.toFixed(2)}</h4>
-      </div>
-      {
-        transactions.length === 0 && (
-          <p>
-            No transactions yet.
-          </p>
-        )
-      }
-      {
-        sortedTransactions.map((transaction) => {
-          return <TransactionCard id={transaction.id} amount={transaction.amount} description={transaction.description} transactionDate={transaction.transactionDate}
-            type={transaction.type} key={transaction.id} />
-        })
-      }
-    </>
+    </div>
   );
 
 
--- finance-dashboard-frontend/src/pages/Dashboard/TransactionCard.tsx
+++ finance-dashboard-frontend/src/pages/Dashboard/TransactionCard.tsx
@@ -2,19 +2,19 @@ import type { Transaction } from "../../types/transaction";
 
 export default function TransactionCard({ amount, description, transactionDate, type }: Transaction) {
   return (
-    <div>
-      <h4>{description}</h4>
-      <p>{transactionDate}</p>
-      <p>{type}</p>
-
-      <strong style={{
-        color:
+    <div className="bg-white rounded-xl shadow p-4 mb-4 flex justify-between items-center">
+      <h3 className="font-semibold">{description}</h3>
+      <p className="text-sm text-gray-500">{transactionDate}</p>
+      <div className="text-right">
+        <p className={
           type === "INCOME"
-            ? "green"
-            : "red"
-      }}>
-        ${amount.toFixed(2)}
-      </strong>
+          ? "text-green-600 font-bold"
+          : "text-red-600 font-bold"
+        }>
+          ${amount.toFixed(2)}
+        </p>
+        <p className="text-sm text-gray-500">{type}</p>
+      </div>
     </div>
   );
 }

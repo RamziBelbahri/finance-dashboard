51704eb39c0c4d952256894271a088e54ad24fae
Ramzi Belbahri <Ramzi.Belbahri@fdmgroup.com>
2026-05-25 13:46:55 -0400

    added frontend login form

--- finance-dashboard-frontend/src/pages/LoginPage.tsx
+++ finance-dashboard-frontend/src/pages/LoginPage.tsx
@@ -1,15 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
 import api from "../api/axios";
+import { useState } from "react";
 
 export default function LoginPage() {
-  const { login, logout } = useAuth();
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const { login } = useAuth();
   const navigate = useNavigate();
 
-  const handleLogin = async () => {
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     try {
-      const email = "test@test.com";
-      const password = "secret123";
       const response = await api.post("/auth/login", { email : email, password: password });
       const jwt = response.data.token;
       login(jwt);
@@ -20,15 +22,82 @@ export default function LoginPage() {
     }
   };
 
-  const handleLogout = async () => {
-    logout();
-  }
-
   return (
-    <div>
-      <h2>Login</h2>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <>
+    <div className="h-full bg-gray-900 flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h1 className="mb-10 text-center text-2xl/9 font-bold tracking-tight text-white">Finance dashboard</h1>
+          <img
+            alt="Your Company"
+            src="./assets/icons/dashboard.png"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleLogin} className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-400">
+            Not a member?{' '}
+            <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
